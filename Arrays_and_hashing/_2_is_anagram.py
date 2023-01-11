7f0bda86190a9cff5f5ec4952bf1dcbb45a91d66
vaibhavlvl1 <acevaibhav14@gmail.com>
2023-01-11 13:42:29 +0530

    .

--- Arrays_and_hashing/_2_is_anagram.py
+++ Arrays_and_hashing/_2_is_anagram.py
@@ -21,25 +21,74 @@ Constraints:
 s and t consist of lowercase English letters.
 """
 
+# dictionary
 def is_anagram(s,t):
-    d = dict()
     if len(s) != len(t):
         return False
-    for ele in s:
-        d[ele] = d.get(ele, 0) + 1
+    d = {}
 
-    for ele in t:
-        if d[ele]==0:
+    for ele in s:
+        if ele not in d:
             d[ele] = 1
         else:
-            d[ele] = d[ele]-1
+            d[ele] += 1
 
+    for ele in t:
+        if ele not in d:
+            d[ele] = 1
+        else:
+            d[ele] -= 1
 
-    print(d)
     for value in d.values():
-        if value >0:
+        if value > 0 or value < 0:
             return False
     return True
 
 
-print(is_anagram('ccaa','cataq'))
+# print(is_anagram('ccaa','cataq'))
+
+# using freq array
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq=[0]*256
+
+        for ele in s:
+            freq[ord(ele)]+=1
+
+        for ele in t:
+            freq[ord(ele)]-=1
+
+        if freq == [0]*256:
+            return True
+        else:
+            return False
+
+
+
+# one liner
+from collections import Counter
+
+def is_anagram(s,t):
+    return Counter(s)==Counter(t)
+
+# using sort
+# sort the strings and equate them
+
+
+# making a count dicts and comparing
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        count_s={}
+        count_t={}
+
+        for i in range(len(s)):
+            count_s[s[i]]= count_s.get(s[i],0)+1
+            count_t[t[i]]= count_t.get(t[i],0)+1
+
+        for ele in s:
+            if count_s[ele]!=count_t.get(ele,0):
+                return False
+        return True
